f7c57ac10333bc6b2dca629404f85fb34b8f4c7e
ashis1234 <57670251+ashis1234@users.noreply.github.com>
2021-06-23 13:19:19 +0530

    Update StudentViews.py

--- student_management_app/StudentViews.py
+++ student_management_app/StudentViews.py
@@ -8,8 +8,7 @@ from django.shortcuts import render
 from django.urls import reverse,reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import Students, Courses, Subjects, CustomUser, Attendance, AttendanceReport, \
-    LeaveReportStudent, FeedBackStudent, NotificationStudent, StudentResult, OnlineClassRoom, SessionYearModel
+from .models import Students, Courses, Subjects, CustomUser, Attendance, AttendanceReport,LeaveReportStudent, FeedBackStudent, StudentResult, SessionYearModel
 
 
 def student_home(request):
@@ -134,21 +133,6 @@ def student_profile_save(request):
             messages.error(request, "Failed to Update Profile")
             return HttpResponseRedirect(reverse("student_profile"))
 
-@csrf_exempt
-def student_fcmtoken_save(request):
-    token=request.POST.get("token")
-    try:
-        student=Students.objects.get(admin=request.user.id)
-        student.fcm_token=token
-        student.save()
-        return HttpResponse("True")
-    except:
-        return HttpResponse("False")
-
-def student_all_notification(request):
-    student=Students.objects.get(admin=request.user.id)
-    notifications=NotificationStudent.objects.filter(student_id=student.id)
-    return render(request,"student_template/all_notification.html",{"notifications":notifications})
 
 def student_view_result(request):
     student=Students.objects.get(admin=request.user.id)
