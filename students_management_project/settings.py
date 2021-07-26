e787a037805912a645a8b4363c5a127dc86e2b99
ashis1234 <57670251+ashis1234@users.noreply.github.com>
2021-07-26 19:19:01 +0530

    Update settings.py

--- students_management_project/settings.py
+++ students_management_project/settings.py
@@ -84,14 +84,14 @@ WSGI_APPLICATION = 'students_management_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE':'django.db.backends.mysql',
-        'NAME':'jgec_management',
-        'USER':'jgec_management',
-        'PASSWORD':'jgec_management',
-        'HOST':'localhost',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE':'django.db.backends.mysql',
+#         'NAME':'jgec_management',
+#         'USER':'jgec_management',
+#         'PASSWORD':'jgec_management',
+#         'HOST':'localhost',
+#         'PORT':'3306'
     }
 }
 
